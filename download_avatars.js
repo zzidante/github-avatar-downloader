63f622196ed7540ba9afc8030b0710759f6af302
Mel <zzidante@gmail.com>
2017-10-10 02:41:01 +0000

    loops over github contributors and urls

--- download_avatars.js
+++ download_avatars.js
@@ -1,63 +1,34 @@
 var request = require('request');
-// var fs = require('fs');
+var codeInput = process.argv.slice(2);
 var GITHUB_USER = "zzidante";
 var GITHUB_TOKEN = "cf00c4d270f935c36bbdfc63af249cab3b1fc983";
 var repoOwner = "jquery";
 var repoName = "jquery";
-var requestURL = 'https://' + GITHUB_USER + ":" + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-
+var requestURL = {
+  url: 'https://' + GITHUB_USER + ":" + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+  headers: {
+    'User-Agent': 'zzidante'
+  }
+};
 
 console.log('Welcome to the Github Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
-  request(requestURL, function(err, response, body){
+
+
+function getRepoContributors(requestURL, cb) {
+  request(requestURL, function(err, response, body) {
     var data = JSON.parse(body);
-  })
+    data.forEach(function(entry) {
+      console.log(`${entry.login}: ${entry.avatar_url}`);
+    });
+  });
+} //bracket ends for function getRepo
 
 
-  // request.get(requestURL)
-  //   .on('error', function (err) {
-  //     console.log("That is not a valid command.");
-  //     throw err;
-  //   })
-  //   .on('response', function (response) {
-  //     console.log(`Response Status Code:  ${response.statusCode}: ${response.statusMessage} - ${response.headers['content-type']}`)
-  //   })
 
-
-}
-
-
-getRepoContributors(repoOwner, repoName, function(err, result) {
+getRepoContributors(requestURL, function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
-});
-
-console.log(requestURL);
+}); //bracket ends here for getRepoContributora
 
 
-
-
-// Reference Code
-
-// var request = require('request');
-// var fs = require('fs');
-
-// request.get('https://sytantris.github.io/http-examples/future.jpg')  // equiv to request()
-//   .on('error', function (err) { // handles any error
-//     console.log("That is not a vaid command");
-//     throw err;
-//   })
-//   .on('data', function (data) { // when data is passed through stream
-//     console.log("Downloading image...");
-//   })
-//   .on('end', function (end) { // end of stream
-//     console.log("Download complete.");
-//   })
-//   .on('response', function (response) {  // handles response
-//     console.log(`Response Status Code:  ${response.statusCode}: ${response.statusMessage} - ${response.headers['content-type']}`);
-//   })
-//   .pipe(fs.createWriteStream('./future.jpg'));
-
-
-//   getRepoContributors("jquery", "jquery");

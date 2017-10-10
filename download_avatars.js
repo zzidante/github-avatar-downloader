d535544a7a63f9cae3dbe6b531dae290ae1c0ad0
Mel <zzidante@gmail.com>
2017-10-10 02:55:34 +0000

    loop and grab username and avatar URL

--- download_avatars.js
+++ download_avatars.js
@@ -16,13 +16,22 @@ console.log('Welcome to the Github Avatar Downloader!');
 
 
 function getRepoContributors(requestURL, cb) {
+  var userObject = {};
   request(requestURL, function(err, response, body) {
     var data = JSON.parse(body);
     data.forEach(function(entry) {
-      console.log(`${entry.login}: ${entry.avatar_url}`);
+      userObject.username = entry.login;
+      userObject.avatar = entry.avatar_url;
+      // console.log(userObject);
+      cb(userObject);
     });
   });
 } //bracket ends for function getRepo
+
+
+function downloadImageByURL(url, filePath) {
+
+}
 
 
 
@@ -32,3 +41,60 @@ getRepoContributors(requestURL, function(err, result) {
 }); //bracket ends here for getRepoContributora
 
 
+
+
+
+// Your next and final step in this exercise should be to change
+// your getRepoContributors function to parse the JSON string into an
+// object and pass this object (an array of contributor objects)
+// to the cb function.
+
+
+
+
+
+
+
+  // request.get(requestURL)
+  //   .on('error', function (err) {
+  //     console.log("That is not a valid command.");
+  //     throw err;
+  //   })
+  //   .on('response', function (response) {
+  //     console.log(`Response Status Code:  ${response.statusCode}: ${response.statusMessage} - ${response.headers['content-type']}`)
+  //   })
+
+
+
+
+
+
+
+// var request = require('request');
+// var code = process.argv.slice(2)[0];
+// var front_url = 'http://coincap.io/front';
+// var coin_url = 'http://coincap.io/page/';
+
+// if (code) {
+//   var url = coin_url + lookup.toUpperCase();
+//   request(url, function(err, res, body) {
+//     var coin = JSON.parse(body);
+
+//     if (coin.id) {
+//       console.log(`${coin.display_name} (${coin.id})`);
+//       console.log("24hr change: ", coin.cap24hrChange);
+//       console.log("USD: $" + coin.price_usd);
+//     } else {
+//       console.log("No coin data found for " + code);
+//     }
+//   });
+// } else {
+//   request(url, function(err, res, body) {
+//     var coins = JSON.parse(body);
+
+//     for (var i = 0; i < coins.length; i++) {
+//       var coin = coins[i];
+//       console.log(`${coin.long} (${coin.short}) - $${coin.price}`);
+//     }
+//   });
+// }

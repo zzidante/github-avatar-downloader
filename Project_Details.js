d9bea12421cbf23591a5a165f331956690adc8f1
Mel <zzidante@gmail.com>
2017-10-10 00:58:13 +0000

    Added Json and .gitignore file

---
+++ Project_Details.js
@@ -0,0 +1,19 @@
+Project_Details.js
+
+// Build a Command Line HTTP Client
+// Requests Avatars for All Contributors to a Given Project
+  // Downloads those Images to Disk
+
+// Given a GitHub Repo Name + Owner
+    // Download all Contributors Profile Images
+    // Save them to Directory avatars/
+        // name of each avatar should be Contributor to REPO
+
+// HOW TO EXECUTE FROM COMMAND LINE
+    // node download_avatars.js GITHUB_REPO GITHUB_OWNER_NAME
+
+
+
+
+// grab Array { avatar_url  }
+// grab {login}

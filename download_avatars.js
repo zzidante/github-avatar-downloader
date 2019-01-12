1eb3276f8621761c273473b011a12279d218fa8f
Mel <zzidante@gmail.com>
2019-01-12 01:14:26 -0800

    Move code to more logical places

--- download_avatars.js
+++ download_avatars.js
@@ -6,27 +6,23 @@ const GITHUB_TOKEN = process.env.GIT_API_KEY;
 const repoOwner = process.argv[2];
 const repoName = process.argv[3];
 
-if (!repoOwner || !repoName) {
-  console.log("You need to specify a Username and Repo name. Try again.");
-  return;
-}
-
-console.log('Welcome to the GitHub Avatar Downloader!\n Type in the name of the Contributor and the Repo name.');
-
-let requestURL = {
-  url: `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos${repoOwner}/${repoName}/contributor`,
-  headers: { 'User-Agent': 'zzidante' },
-};
-
 function getRepoContributors(requestURL, cb) {
+  if (!repoOwner || !repoName) {
+    console.log("You need to specify a Username and Repo name. Try again.");
+    return;
+  }
+  
+  console.log("Welcome to the GitHub Avatar Downloader!\n Type in the name of the Contributor and the Repo name.");
+
   let userObject = {};
+
   request(requestURL, (_err, _res, body) => {
     const data = JSON.parse(body);
 
     if(!fs.existsSync("./avatars/")) {
       fs.mkdirSync("./avatars/");
     }
-    
+
     data.forEach((entry) => {
       userObject.username = entry.login;
       userObject.avatar = entry.avatar_url;
@@ -46,4 +42,10 @@ function downloadImageByUrl(userObject) {
     .pipe(fs.createWriteStream(`./avatars/${userObject.username}.jpg`));
 }
 
-getRepoContributors(requestURL, downloadImageByUrl);  // initiate the process
+let requestURL = {
+  url: `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos${repoOwner}/${repoName}/contributor`,
+  headers: { 'User-Agent': 'zzidante' },
+};
+
+// initiate the process
+getRepoContributors(requestURL, downloadImageByUrl);

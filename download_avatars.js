68c15bb1415793f1adfea5d5d9f3c7c182bf9dfb
Mel <zzidante@gmail.com>
2019-01-12 01:05:03 -0800

    ES6 it

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,6 @@
 const request = require('request');
 const fs = require('fs');
-const dotev = require('dotenv/config')
+const dotev = require('dotenv/config');
 const GITHUB_USER = "zzidante";
 const GITHUB_TOKEN = process.env.GIT_API_KEY;
 const repoOwner = process.argv[2];
@@ -8,43 +8,42 @@ const repoName = process.argv[3];
 
 if (!repoOwner || !repoName) {
   console.log("You need to specify a Username and Repo name. Try again.");
-    return;
+  return;
 }
 
+console.log('Welcome to the GitHub Avatar Downloader!\n Type in the name of the Contributor and the Repo name.');
+
 let requestURL = {
-  url: 'https://' + GITHUB_USER + ":" + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
-  headers: {
-    'User-Agent': 'zzidante'
-  }
+  url: `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos${repoOwner}/${repoName}/contributor`,
+  headers: { 'User-Agent': 'zzidante' },
 };
 
-console.log('Welcome to the GitHub Avatar Downloader!');
-// console.log('Make sure you type in the name of the Contributor and then the Repo name.')
-
 function getRepoContributors(requestURL, cb) {
-  let userObject = {}
-  request(requestURL, function(err, res, body) {
+  let userObject = {};
+  request(requestURL, (_err, _res, body) => {
     const data = JSON.parse(body);
+
     if(!fs.existsSync("./avatars/")) {
-      fs.mkdirSync("./avatars/")
+      fs.mkdirSync("./avatars/");
     }
-    data.forEach(function(entry){
+    
+    data.forEach((entry) => {
       userObject.username = entry.login;
       userObject.avatar = entry.avatar_url;
-      cb(userObject)
-    })
-  })
+      cb(userObject);
+    });
+  });
 }
 
 function downloadImageByUrl(userObject) {
   request.get(userObject.avatar)
-  .on('error', function (err){
-    throw err;
-  })
-  .on('end', function (end){
-    console.log("Download Finished")
-  })
-  .pipe(fs.createWriteStream('./avatars/' + userObject.username + '.jpg'))
+    .on('error', (err) => {
+      throw err;
+    })
+    .on('end', (_end) => {
+      console.log("Download Finished");
+    })
+    .pipe(fs.createWriteStream(`./avatars/${userObject.username}.jpg`));
 }
 
-getRepoContributors(requestURL, downloadImageByUrl)  // initiate the process
+getRepoContributors(requestURL, downloadImageByUrl);  // initiate the process
